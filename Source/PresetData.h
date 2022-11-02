98a7be048758b8d6f771afe14888de672d37f747
Scott Clayton <97351459+backpain22@users.noreply.github.com>
2022-11-01 17:05:24 -0700

    Add files via upload

---
+++ Source/PresetData.h
@@ -0,0 +1,31 @@
+/* (Auto-generated binary data file). */
+
+#ifndef BINARY_PRESETDATA_H
+#define BINARY_PRESETDATA_H
+
+namespace PresetData
+{
+    extern const char*  externalFiles;
+    const int           externalFilesSize = 1146;
+
+    extern const char*  images;
+    const int           imagesSize = 332203;
+
+    extern const char*  impulses;
+    const int           impulsesSize = 28;
+
+    extern const char*  midiFiles;
+    const int           midiFilesSize = 28;
+
+    extern const char*  preset;
+    const int           presetSize = 3528;
+
+    extern const char*  samplemaps;
+    const int           samplemapsSize = 28;
+
+    extern const char*  userPresets;
+    const int           userPresetsSize = 1317;
+
+}
+
+#endif
